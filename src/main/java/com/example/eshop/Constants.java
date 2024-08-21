5505145a225827f7e11cd5b3b919e9c5dfaf3c2d
Darius <darius.songaila1@gmail.com>
2024-08-21 14:45:13 +0300

    a

--- src/main/java/com/example/eshop/Constants.java
+++ src/main/java/com/example/eshop/Constants.java
@@ -1,9 +1,9 @@
 package com.example.eshop;
 
 public class Constants {
-    public final static String URL = "jdbc:mysql://host.docker.internal:3308/eshop";
+    public final static String URL = "jdbc:mysql://host.docker.internal:3307/eshop";
     public final static String USERNAME = "root";
-    public final static String PASSWORD = "1234";
+    public final static String PASSWORD = "root";
 
     private Constants() {
         throw new AssertionError("Cannot instantiate the Constants class");
