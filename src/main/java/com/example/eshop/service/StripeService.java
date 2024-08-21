380b74d590e55784baca0028a3a407e7e0c0959b
Darius <darius.songaila1@gmail.com>
2024-08-21 17:44:10 +0300

    a

--- src/main/java/com/example/eshop/service/StripeService.java
+++ src/main/java/com/example/eshop/service/StripeService.java
@@ -15,8 +15,9 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-
+@Service
 public class StripeService {
+
 
     OrderService orderService = new OrderService();
 
